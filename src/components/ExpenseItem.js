dfe89dc59245103d7d15ff81d2c7859de55239fa
chanz-pclimaco <pclimaco@chanzglobal.com>
2023-10-11 15:03:25 +0800

    Outputting Dynamic Data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+    const expenseDate = new Date(2023, 3, 9);
+    const expenseTitle = "Car Insurance";
+    const expenseAmount = 294.67;
+
     return (
         <div className='expense-item'>
-            <div>January 01, 2023</div>
+            <div>{ expenseDate.toISOString() }</div>
             <div className='expense-item__description'>
-                <h2>Car Insurance</h2>
-                <div className='expense-item__price'>Php 294.67</div>
+                <h2>{ expenseTitle }</h2>
+                <div className='expense-item__price'>Php { expenseAmount }</div>
             </div>
         </div>
     );
