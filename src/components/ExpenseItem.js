54a4640b9634484525e1167f5c40edd07e7dfa2c
chanz-pclimaco <pclimaco@chanzglobal.com>
2023-10-11 14:52:16 +0800

    Styling a React Component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
     return (
-        <div>
+        <div className='expense-item'>
             <div>January 01, 2023</div>
-            <div>
+            <div className='expense-item__description'>
                 <h2>Car Insurance</h2>
-                <div>Php 294.67</div>
+                <div className='expense-item__price'>Php 294.67</div>
             </div>
         </div>
     );
