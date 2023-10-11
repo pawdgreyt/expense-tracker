7475686a287ff88975c7d1c1ed20254f087dce99
chanz-pclimaco <pclimaco@chanzglobal.com>
2023-10-11 14:46:56 +0800

    Initial Commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>January 01, 2023</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>Php 294.67</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
